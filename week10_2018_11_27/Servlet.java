3924fdceb043656b777407bb84c3ead2cddd554e
jnieto7 <jnieto7@users.noreply.github.com>
2018-11-28 23:00:35 -0500

    Update Servlet.java

--- week10_2018_11_27/Servlet.java
+++ week10_2018_11_27/Servlet.java
@@ -1,21 +1,21 @@
 /*
 * Jeremie Nieto
  */
-package servlet;
+package jn.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 /**
  * Right now this is a test Servlet that is not fully implemented
- * 
+ *
  * @author jnieto
  */
+@WebServlet("/processOptions")
 public class Servlet extends HttpServlet {
-
-    private final String message = "TCPDUMP EXECUTOR";
 
     @Override
     public void init() throws ServletException {
@@ -30,18 +30,38 @@ public class Servlet extends HttpServlet {
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        out.println("<h1>" + this.message + "</h1>");
+        out.println("<h1>" + "No Content" + "</h1>");
     }
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String captureInterface = request.getParameter("interface");
+
+        StringBuilder command = new StringBuilder("/usr/sbin/tcpdump -i ").append(captureInterface);
+
+        String lenBytes = request.getParameter("lenBytes");
+        if (lenBytes != null) {
+            command.append(" -s ").append(lenBytes);
+        }
+
+        String sourceHost = request.getParameter("sourceHost");
+        String sourcePort = request.getParameter("sourcePort");
+
+        if (sourcePort != null) {
+
+        }
+
+        String destHost = request.getParameter("destHost");
+        String destPort = request.getParameter("destPort");
+
         // Set response content type
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        out.println("<h1>" + this.message + "</h1>");
+        out.println("<h1>" + "running with command: " + "</h1>");
+        out.println("<h1>" + "TCPDumps successfully captured!" + "</h1>");
     }
 
     @Override
