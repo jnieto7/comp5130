86c1378056affe937091363b90c956f7dce01f7b
jnieto7 <jnieto7@users.noreply.github.com>
2018-12-04 01:13:31 -0500

    Update Server.java

--- final_project/Server.java
+++ final_project/Server.java
@@ -4,30 +4,45 @@
  */
 package jn.servlet;
 
+import java.io.IOException;
+
 /**
+ * The Server class holds the user information for making a connection to a
+ * remote host.
  *
  * @author jnieto
  */
-class Server {
+class Server extends Thread {
 
     private final String host;
     private final String user;
     private final String password;
+    private CommandType type = CommandType.SSH;
+    private String command;
+    private String results = "";
+
+    enum CommandType {
+        LOCAL,
+        SSH
+    }
 
     Server(String name, String user, String password) {
+        super.setDaemon(true);
         this.host = name;
         this.user = user;
         this.password = password;
     }
 
     Server(String name, String password) {
-        this.host = name;
-        this.user = "root";
-        this.password = password;
+        this(name, "root", password);
     }
 
-    String executeSSHCommand(String command) throws Exception {
-        return ServerCommand.executeSSHCommand(this.host, this.user, this.password, command);
+    void executeLocalCommand(String command) throws IOException, InterruptedException {
+        this.results = ServerCommand.executeLocalCommand(command, 20000);
+    }
+
+    void executeSSHCommand(String command) throws Exception {
+        this.results = ServerCommand.executeSSHCommand(this.host, this.user, this.password, command);
     }
 
     String getHost() {
@@ -37,9 +52,35 @@ class Server {
     String getPassword() {
         return this.password;
     }
+    
+    String getResults(){
+        return this.results;
+    }
 
     String getUser() {
         return this.user;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (this.type == CommandType.LOCAL) {
+                executeLocalCommand(this.command);
+            } else {
+                executeSSHCommand(this.command);
+            }
+        } catch (Throwable th) {
+            throw new RuntimeException(th);
+        }
+
+    }
+
+    void setCommand(String command) {
+        this.command = command;
+    }
+
+    void setCommandType(CommandType type) {
+        this.type = type;
     }
 
     @Override
